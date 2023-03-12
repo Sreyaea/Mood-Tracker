a50dc1bfdb252473c603cc98f11f95ce97e6522c
unknown <diyadominic301202@gmail.com>
2023-03-12 12:40:13 +0530

    first

---
+++ devcraftproject/src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import Button from 'react-bootstrap/Button';
+export default function Home() {
+  return (
+    <div className='body'>
+        <div className='text'>
+<h1>Who are you today ?</h1>
+<Button variant="outline-primary">sad</Button>{' '}
+        </div>
+        </div>
+  )
+}
